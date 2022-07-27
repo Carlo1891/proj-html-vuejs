01d7b7e19bb283f4e42762dbf0e32dc76f540a88
Carlo1891 <carlo.18@live.it>
2022-07-27 23:28:02 +0200

    feat: Add main and jumbotron section

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,8 +1,48 @@
 <template>
   <main>
+    <div class="pre-jumbo d-flex justify-content-around">
 
-    Contenuto
-    
+      <div>
+        <ul>
+
+          <li>
+            <i class="fa-solid fa-city"></i>
+            Business
+          </li>
+
+          <li>
+            <i class="fa-solid fa-palette"></i>
+            Design
+          </li>
+        
+          <li>
+            <i class="fa-solid fa-gear"></i>
+            Development
+          </li>
+        
+          <li>
+            <i class="fa-solid fa-face-smile"></i>
+            Lifestyle
+          </li>
+        
+          <li>
+            <i class="fa-solid fa-folder-open"></i>
+            Office Productivity
+          </li>
+        </ul>
+
+      </div>
+    </div>
+
+    <div>
+      <figure class="jumbotron d-flex">
+        <h1 class="text-white">
+          Udemy Affiliate Sales
+        </h1>
+
+        <img src="../../../public/img/slide-1.jpg" alt="slide-1" class="img-fluid">
+      </figure>
+    </div>
   </main>
 </template>
 
@@ -17,6 +57,21 @@
 
 <style scoped lang="scss">
 
+  @import "../../assets/styles/variables.scss";
 
+  .pre-jumbo {
+    margin: 0 auto;
+    width: 70%;
+  }
 
+  div>ul {
+    list-style: none;
+  }
+
+  ul>li {
+    display: inline-block;
+  }
+  .jumbotron{
+    background-color: $bgJumbotron;
+  }
 </style>
